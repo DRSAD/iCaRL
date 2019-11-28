7fc594cdcb0607cf0f0e26b6191762fea86029f3
zhuoyunli <30347144+zhuoyunli@users.noreply.github.com>
2019-11-28 14:47:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ batch_size=128
 task_size=10
 memory_size=2000
 epochs=70
-learning_rate=2.0
+learning_rate=0.1
 
-model=iCaRLmodel(numclass,feature_extractor,img_size,batch_size,task_size,memory_size,epochs,learning_rate)
+model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate)
+#model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
 
 for i in range(10):
     model.beforeTrain()
     accuracy=model.train()
-    KNN_accuracy=model.afterTrain()
-    print("NMS准确率："+str(KNN_accuracy.item()))
-    torch.save(model.model.state_dict(),'model/accuracy:%.3f_KNN_accuracy:%.3f_increment:%d_net.pkl' % (accuracy, KNN_accuracy, i + 10))
+    model.afterTrain(accuracy)
