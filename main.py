d76a3d71ac2118b10c62282c34e9fe4a77eebadd
zhuoyunli <30347144+zhuoyunli@users.noreply.github.com>
2019-12-26 10:10:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ batch_size=128
 task_size=10
 memory_size=2000
 epochs=70
-learning_rate=0.1
+learning_rate=2.0
 
 model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate)
 #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
